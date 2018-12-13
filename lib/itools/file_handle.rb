d9914a94d6ef227f09f484a85f7ca3a859afdfd6
张贵 <zhanggui@maoyan.com>
2018-12-13 19:30:55 +0800

    opt excel style

--- lib/itools/file_handle.rb
+++ lib/itools/file_handle.rb
@@ -1,4 +1,5 @@
 require 'find'
+require 'spreadsheet'
 module Itools
     class FileResult
         attr_accessor :keyword, :file_path, :file_name
@@ -16,16 +17,17 @@ module Itools
             @search_result = []
         end
         def search 
-            puts @files
+            puts "\033[32m开始查找...\033[0m"    
             if File::directory?(@path)
                 Find.find(@path) do |file|
                     if File.file?(file)
                         file_name = File.basename(file)
-                        if file_name.include?("ViewController.m")
+                        if file_name.include?(@files)
                             fr = FileResult.new(file,file_name)
                             @search_result << fr
                         end
-                        
+                    else
+                        # puts "查找#{file}..."
                     end
                 end
             else
@@ -40,15 +42,37 @@ module Itools
                 puts "\033[31m参数异常，请传入两个参数，第一个为路径，第二个为要搜索的文件名\033[0m"
                 return
             end
-           temp_files = files.split(",")
-            file_searcher = FileSearcher.new(path,temp_files)
+        #    temp_files = files.split(",")
+            file_searcher = FileSearcher.new(path,files)
             file_searcher.search
+            if file_searcher.search_result.size == 0
+                puts "\033[32m没有找到符合条件的文件\033[0m"    
+                return
+            end
             # 输出
-            puts "\033[32m找到共#{file_searcher.search_result.size}个文件结果如下；\033[0m"    
-            file_searcher.search_result.each {|item| 
-                puts item.file_name + "-----"+item.file_path
-            }
+              # 输出搜索的内容
+        
+              Spreadsheet.client_encoding = 'utf-8'
+              book = Spreadsheet::Workbook.new
+              sheet1 = book.create_worksheet
+              sheet1.row(0)[0] = "序号"
+              sheet1.row(0)[1] = "文件名"
+              sheet1.row(0)[2] = "文件所在路径"
+              
             
+              puts "\033[32m找到共#{file_searcher.search_result.size}个文件结果如下；\033[0m"    
+              file_searcher.search_result.each_with_index {|item,i| 
+                puts item.file_name
+                sheet1.row(i+1)[0] = i + 1
+                sheet1.row(i+1)[1] = item.file_name
+                sheet1.row(i+1)[2] = item.file_path
+                sheet1.row(i+1).height = 20
+            } 
+            sheet1.column(0).width = 4
+            sheet1.column(1).width = 45
+            sheet1.column(2).width = 100
+            book.write "#{File.dirname(path)}/search_#{files}_result.xls"
+            puts "\033[32m查找成功,共#{file_searcher.search_result.size}个文件,内容已经保存到#{File.dirname(path)}/search_#{files}_result.xls，请点击查看\033[0m"
         end
     end
 end
